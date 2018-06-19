b12ba87aa9271c817cfac7336a9b93ff3ec67be5
meng.zhaoyang <meng.zhaoyang@bothivesystems.com>
2018-06-19 10:38:40 +0800

    update

---
+++ others/logdemo/src/main/java/com/yksdy/logdemo/App.java
@@ -0,0 +1,13 @@
+package com.yksdy.logdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
